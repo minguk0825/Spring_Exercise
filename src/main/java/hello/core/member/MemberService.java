31faacc225ccd13a0310e30763e442edffd3ee17
minguk0825 <mapletrue01@naver.com>
2024-01-09 22:01:44 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/MemberService.java
@@ -0,0 +1,11 @@
+package hello.core.member;
+
+public class MemberService {
+
+    public void join(Member member) {
+
+    }
+    public Member findMember(Long memberId) {
+        return null;
+    }
+}
